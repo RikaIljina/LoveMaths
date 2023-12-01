712071a13a7b7cc26864ddd421ef7dba9e59992f
Vasilika Schnitzer <wasilika@hotmail.com>
2023-12-01 18:08:15 +0100

    Add code to addition function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert("You clicked submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(gameType);
+                runGame(gameType);
             }
         })
     }
@@ -19,9 +19,16 @@ document.addEventListener('DOMContentLoaded', function() {
  * The main game loop, called when the script is first loaded
  * and after the user selects a play mode
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting.`;
+    }
 
 }
 
@@ -41,7 +48,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
