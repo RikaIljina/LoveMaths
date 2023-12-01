f75aeb554d27ebc3388206a992e21b8cb9ed7fd9
Vasilika Schnitzer <wasilika@hotmail.com>
2023-12-01 17:57:25 +0100

    Add code to calculate two random numbers for the game loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
+// Wait for DOM content to load before starting the game
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit" ) {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(gameType);
+            }
+        })
+    }
+
+})
+
+/**
+ * The main game loop, called when the script is first loaded
+ * and after the user selects a play mode
+ */
 function runGame() {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
 
